bb95fde98cb1291e9f0d1aa2a2e22128aeb4984a
Mfundo <jordantenza@gmai.com>
2021-11-07 10:07:28 +0200

    Set up databinding and set Activity to observe ViewModel

--- app/src/main/java/com/mfundoza/counter/MainActivity.java
+++ app/src/main/java/com/mfundoza/counter/MainActivity.java
@@ -1,14 +1,35 @@
 package com.mfundoza.counter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 
+import com.mfundoza.counter.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    MainViewModel mainViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // setContentView(R.layout.activity_main);
+
+        mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        binding.setLifecycleOwner(this);
+        binding.setViewmodel(mainViewModel);
+
+        mainViewModel.getCount().observe(this, new Observer() {
+            @Override
+            public void onChanged(Object o) {
+                binding.txtCount.setText(o.toString());
+            }
+        });
+
     }
 }
